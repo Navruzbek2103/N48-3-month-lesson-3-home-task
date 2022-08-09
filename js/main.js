a5f58b38acb8258b6a2aa8a414b0580685db5c3b
Navruzbek2103 <navrozbek2306@gmail.com>
2022-08-10 01:08:55 +0500

    Task 3

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// 1-masala
+// a nomli o'zgaruvchi e'lon qiling unga 10 raqam qiymat biriktiring, b degan o'zgaruvchi oching unga 5 ni biriktiring va console ga sonlarni yig'indisini chiqaring.
+
+
+
+// var a = 10;
+// var b = 5;
+// console.log(a + b);
+
+
+// _____________________________________________________________________
+
+
+// 2-masala.
+// a nomli o'zgaruvchi e'lon qiling unga 25 raqam qiymat biriktiring, b degan o'zgaruvchi oching unga 15 ni biriktiring va natijani result degan o'zgaruchiga oling (a dan b ni ayring), consolega result ni chiqaring.
+
+// var a = 25;
+// var b = 15;
+// var result = (a - b)
+// console.log(result);
+
+
+// _____________________________________________________________________
+
+
+// 3-masala.
+// 3. a nomli o'zgaruvchi e'lon qiling unga 10 ni biriktiring b nomli o'zgaruvchi e'lon qiling va unga 5 ni biriktiring keyin a dan b ni ayring natijani c nomli o'zgaruvchiga chiqaring. Keyin d nomli o'zgaruvchi e'lon qilib unga 7 ni biriktiring c va d ni biriktiring va natijani result nomli o'zgaruvchida console ga chiqaring.
+
+// var a = 10;
+// var b = 5;
+// var c = a - b;
+// var d = 7;
+// var result = c + d;
+// console.log(result);
+
+
+// _____________________________________________________________________
+
+
+// 4-masala.
+// a nomli o'zgaruvchi e'lon qiling unga 10 ni biriktiring b o'zgaruvchiga 3 ni biriktiring va console ga qoldig'ini chiqaring.
+
+// var a = 10;
+// var b = 3;
+// console.log(a % b);
+
+
+// _____________________________________________________________________
+
+
+// 5-masala.
+// 2 ning 10 chi darajasini console ga chiqaring
+// console.log(2 ** 10);
+
+
+// _____________________________________________________________________
+
+
+// Og'zaki:
+// 1) 10
+// 2) 8
+// 3) 36
+// 4) 7
+// 5) 13
+// 6) 36
+// 7) 1
+// 8) -1
+// 9) 72
+// 10) 24
+// 11) 216
+
